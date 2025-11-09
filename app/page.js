b7c087c69f9fa9bedfd5e00b18fb99d5a7aea06e
ankit <fabankitkumar@gmail.com>
2025-11-09 15:28:18 +0530

    Firebase DB Setup

--- app/page.js
+++ app/page.js
@@ -2,15 +2,13 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
   const {setTheme}=useTheme();
   return (
     <div>
-      <h2>Hii</h2>
-      <Button>Subscribe</Button>
-      <Button onClick={()=>setTheme('dark')}>Dark Mode</Button>
-      <Button onClick={()=>setTheme('light')}>Light Mode</Button>
+      <ChatInputBox/>
     </div>
   );
 }

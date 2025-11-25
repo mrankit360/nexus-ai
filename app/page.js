868928c0e988835fcf7df089327e27ea5693249a
ankit <fabankitkumar@gmail.com>
2025-11-25 18:23:20 +0530

    All done

--- app/page.js
+++ app/page.js
@@ -2,13 +2,14 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import { Suspense } from "react";
 import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
   const {setTheme}=useTheme();
   return (
-    <div>
+    <Suspense>
       <ChatInputBox/>
-    </div>
+    </Suspense>
   );
 }
